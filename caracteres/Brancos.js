b8cc288f5dc9c3b7626178b52a870fc1a75b7ee0
Sergio Souza Novak <serginhosnovak@hotmail.com>
2021-03-30 15:04:48 -0300

    feito Caracteres

---
+++ caracteres/Brancos.js
@@ -0,0 +1,8 @@
+const texto = `
+ca  r
+r   o s!
+`
+
+
+console.log(texto.match(/\nca\s\sr\nr\s\s\so\ss!/))
+
---
+++ caracteres/ProblemaPonto.js
@@ -0,0 +1,9 @@
+const texto ="Bom\tdia"
+console.log(texto.match(/.../gi)) //ponto não engloba a quebra de linha 
+
+
+
+// aaabaaa
+// aaaaaaa
+
+// aaa
